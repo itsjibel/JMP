59877c6dcf526c58a601b5170fde768a2beb2b0e
Jibel <itsjibel@gmail.com>
2023-04-12 11:19:04 +0330

    Add stdout for the object

--- src/JMP.h
+++ src/JMP.h
@@ -1,3 +1,20 @@
+#include <vector>
+#include <string.h>
+#include <algorithm>
+using std::string;
+using std::ostream;
+
 class JMP
 {
+    public:
+        string number;
+        // Constructors
+        JMP(string num) { number = num; }
+        JMP() {}
+
+        friend ostream & operator << (ostream &k, JMP &j)
+        {
+            k << j.number;
+            return (k);
+        }
 };
