5f9dd1c63e8fcdf7b0dc63c4edb5a96d7aa70fdf
Jibel <itsjibel@gmail.com>
2023-04-12 21:24:05 +0330

    Add couting the JMP object++

--- src/example.cpp
+++ src/example.cpp
@@ -5,16 +5,19 @@ using std::cout;
 
 int main()
 {
-    JMP j = "1000";
-    cout<<j<<endl;
+    JMP my_number = "1000";
+    cout<<my_number<<endl;
 
-    j = "+80090";
-    cout<<j<<endl;
+    my_number = "+80090";
+    cout<<my_number<<endl;
 
-    string str="-990";
-    j = str;
-    j++;
-    cout<<j<<endl;
+    string str = "-990";
+    my_number = str;
+    my_number++;
+    cout<<my_number<<endl;
+
+    my_number = "+999";
+    cout<<my_number++;
 
     return 0;
 }
