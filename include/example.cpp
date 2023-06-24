ac0c65adf56c5362aef3adcc6685ff86f48ef53d
Jibel <itsjibel@gmail.com>
2023-06-24 11:45:41 +0330

    Add Exponentiation example.

--- include/example.cpp
+++ include/example.cpp
@@ -24,19 +24,9 @@ int main()
     // Calculate Greatest Common Denominator
     std::cout<<"GCD: "<<JMP::gcd(a, b)<<std::endl;
 
-    // Put the jmp number into the string
-    std::string jmp_string = a;
-    std::cout<<jmp_string<<std::endl;
-
-    // Get values from jmp array
-    jmp jarr[5] {"18", "Hello", "9", "World", "9"};
-    for (std::string itr : jarr)
-        std::cout<<itr<<"\t";
-    std::cout<<std::endl;
-    // or ...
-    for (auto i{0}; i<5; i++)
-        std::cout<<jarr[i]<<"\t";
-    std::cout<<std::endl;
+    // Calculate exponentiation of two jmp numbers
+    jmp c("2"), d("-3");
+    std::cout<<"Exponentiation: "<<c.powof(d)<<std::endl;
 
     return 0;
 }
